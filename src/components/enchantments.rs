180332f02319c145d92da90b1775b6a9db47d4c1
VilleOlof <villeolof@gmail.com>
2024-07-31 01:29:29 +0200

    Made enchantment component more serde compat

--- src/components/enchantments.rs
+++ src/components/enchantments.rs
@@ -7,7 +7,7 @@ use crate::{kv::KVPair, traits::FromCompoundNbt};
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Enchantments {
     /// Contains key-value pairs of levels of enchantments on this item that affect the way the item works.  
-    pub enchantments: KVPair<i32>,
+    pub levels: KVPair<i32>,
 
     /// Show or hide enchantments on this item's tooltip. Defaults to true.
     pub show_in_tooltip: bool,
@@ -23,20 +23,20 @@ impl FromCompoundNbt for Enchantments {
         if let Some(levels) = nbt.compound("levels") {
             // Field compound
 
-            let enchantments = KVPair::<i32>::from_compound_nbt(&levels)?;
+            let levels = KVPair::<i32>::from_compound_nbt(&levels)?;
             let show_in_tooltip = nbt.byte("show_in_tooltip").map(|b| b != 0).unwrap_or(true);
 
             return Ok(Enchantments {
-                enchantments,
+                levels,
                 show_in_tooltip,
             });
         } else {
             // key value only
-            let enchantments = KVPair::<i32>::from_compound_nbt(&nbt)?;
+            let levels = KVPair::<i32>::from_compound_nbt(&nbt)?;
 
             // this could be an enum but eh, show_in_tooltip is default true anyway
             return Ok(Enchantments {
-                enchantments,
+                levels,
                 show_in_tooltip: true,
             });
         };
