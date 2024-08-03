23cf1b21ee16ec0a270459cfffdde1b6d7f83227
VilleOlof <villeolof@gmail.com>
2024-08-03 14:11:05 +0200

    Fixed fireworks not defaulting to empty vec in explosions

--- src/components/fireworks.rs
+++ src/components/fireworks.rs
@@ -1,7 +1,7 @@
 //! Firework rocket item component.
 
 use super::firework_explosion::FireworkExplosion;
-use crate::{error::SculkParseError, traits::FromCompoundNbt, util::get_t_list};
+use crate::{traits::FromCompoundNbt, util::get_t_list};
 
 #[cfg(feature = "serde")]
 fn default_flight_duration() -> i8 {
@@ -15,7 +15,7 @@ pub struct Fireworks {
     /// List of the explosion effects caused by this [firework rocket](https://minecraft.wiki/w/Firework_rocket). Has a maximum of 256 explosions.  
     pub explosions: Vec<FireworkExplosion>,
 
-    /// The flight duration of this firework rocket, i.e. the number of gunpowders used to craft it. Must be an integer between -128 and 127. Defaults to 1.
+    /// The flight duration of this firework rocket, i.e. the number of gunpowders used to craft it. Must be an integer between -128 and 127. Defaults to 1.  
     #[cfg_attr(feature = "serde", serde(default = "default_flight_duration"))]
     pub flight_duration: i8,
 }
@@ -27,14 +27,17 @@ impl FromCompoundNbt for Fireworks {
     where
         Self: Sized,
     {
-        let explosions = get_t_list(
-            &nbt.list("explosions")
-                .ok_or(SculkParseError::MissingField("explosions".into()))?,
-            "explosions",
-            FireworkExplosion::from_compound_nbt,
-        )?;
+        let explosions = if let Some(explosions) = &nbt.list("explosions") {
+            get_t_list(
+                explosions,
+                "explosions",
+                FireworkExplosion::from_compound_nbt,
+            )?
+        } else {
+            vec![]
+        };
 
-        let flight_duration = nbt.byte("Flight").unwrap_or(1);
+        let flight_duration = nbt.byte("flight_duration").unwrap_or(1);
 
         Ok(Fireworks {
             explosions,
